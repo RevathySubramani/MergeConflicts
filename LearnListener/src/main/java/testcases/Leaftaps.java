919c84779233eaf82a21ed1539e4af27517a6ace
ASUS <ASUS@DESKTOP-F9UBQTC>
2023-12-28 18:38:17 +0530

    java classes

---
+++ LearnListener/src/main/java/testcases/Leaftaps.java
@@ -0,0 +1,59 @@
+package testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class Leaftaps {
+	public ChromeDriver driver;
+	@BeforeTest
+	public void login() {
+	
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+
+    @Test
+	public  void create() {
+    	driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+		driver.findElement(By.id("createLeadForm_firstNam")).sendKeys("Babu");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("M");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+
+	}
+    @Test
+	public void editLead() throws InterruptedException {
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phon']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("91");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys("TCS");
+		driver.findElement(By.name("submitButton")).click();
+
+
+}
+    @AfterTest
+    public void close() {
+		driver.close();
+
+	}
+    }
